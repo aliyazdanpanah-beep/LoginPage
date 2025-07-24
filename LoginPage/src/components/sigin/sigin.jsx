3f4bf904771cf3eb1c2eb9ce9c6ca218c14bf9cd
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-07-25 00:18:51 +0330

    add button

--- LoginPage/src/components/sigin/sigin.jsx
+++ LoginPage/src/components/sigin/sigin.jsx
@@ -4,27 +4,36 @@ const Sigin = () => {
   return (
     <div className="sigin">
       <form>
-        {/* <div className="wrapper">
-          <input
-            className="name"
-            type="text"
-            placeholder="Firstname"
-            required
-            name="first"
-          />
-          <input
-            className="name"
-            type="text"
-            placeholder="Lastname"
-            required
-            name="last"
-          />
-        </div> */}
-
-        <input className="INP" type="text" placeholder="Username" required name="user"/>
-        <input className="INP" type="email" placeholder="Email" required name="email" />
-        <input className="INP" type="password" placeholder="Password" required name="pass"/>
-        <input className="pass" type="password" placeholder="Secend Password" required name="secpass" />
+        <h3 className="tittil">SignUp an account</h3>
+        <input
+          className="INP"
+          type="text"
+          placeholder="Username"
+          required
+          name="user"
+        />
+        <input
+          className="INP"
+          type="email"
+          placeholder="Email"
+          required
+          name="email"
+        />
+        <input
+          className="INP"
+          type="password"
+          placeholder="Password"
+          required
+          name="pass"
+        />
+        <input
+          className="INP"
+          type="password"
+          placeholder="Secend Password"
+          required
+          name="secpass"
+        />
+        <input id="SUBMIT" type="submit" value={"signUp an account"} />
       </form>
     </div>
   );
