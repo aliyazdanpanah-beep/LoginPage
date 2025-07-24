a15687c0657e128a98a9c247851b630a89c18ed7
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-07-24 21:52:57 +0330

    last login style

--- LoginPage/src/components/login/login.jsx
+++ LoginPage/src/components/login/login.jsx
@@ -4,8 +4,12 @@ const Login = () => {
   return (
     <div className="login">
       <form className="form">
-        <input type="text" placeholder="Email" required name="email" />
+        <h3 className="titel">Login an accunt</h3>
+        <input type="email" placeholder="Email" required name="email" />
+        <input type="text" placeholder="Username" required name="user"/>
         <input type="password" placeholder="Password" required name="pass" />
+        <p id="Create">create an accunt ?</p>
+        <input id="Sub" type="submit" value={"Login an accunt"} />
       </form>
     </div>
   )
