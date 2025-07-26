948be43a376d131e60579e861fce4c256fe7a27b
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-07-26 20:43:33 +0330

    addinh json server

--- LoginPage/src/App.jsx
+++ LoginPage/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import Image from "./components/image/image";
-import Login from "./components/login/login";
 import Home from "./page/Home/home";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="contaner">
       <Image/>
       <Home/>
-      {/* <Login /> */}
     </div>
   );
 }
