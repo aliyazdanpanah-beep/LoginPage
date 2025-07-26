7b382818ce3319b177d22f1588756f36d513820a
Call me Ali <aliyazdanpanahfard@gmail.com>
2025-07-26 20:40:03 +0330

    add page file

--- LoginPage/src/App.jsx
+++ LoginPage/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Image from "./components/image/image";
 import Login from "./components/login/login";
+import Home from "./page/Home/home";
 
 function App() {
   return (
     <div className="contaner">
       <Image/>
-      <Login />
+      <Home/>
+      {/* <Login /> */}
     </div>
   );
 }
---
+++ LoginPage/src/page/Home/home.jsx
@@ -0,0 +1,9 @@
+import Login from "../../components/login/login"
+
+const Home = () => {
+  return (
+     <Login />
+  )
+}
+
+export default Home
